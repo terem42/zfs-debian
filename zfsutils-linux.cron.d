be166c391de9ee4ff6b61f5a8bf19c4a3f18ccbe
terem42 <9478806+terem42@users.noreply.github.com>
2022-06-17 09:56:04 +0200

    applied debteam patches

--- zfsutils-linux.cron.d
+++ zfsutils-linux.cron.d
@@ -1,4 +1,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
+# TRIM the first Sunday of every month.
+24 0 1-7 * * root if [ $(date +\%w) -eq 0 ] && [ -x /usr/lib/zfs-linux/trim ]; then /usr/lib/zfs-linux/trim; fi
+
 # Scrub the second Sunday of every month.
-24 0 8-14 * * root [ $(date +\%w) -eq 0 ] && [ -x /usr/lib/zfs-linux/scrub ] && /usr/lib/zfs-linux/scrub
+24 0 8-14 * * root if [ $(date +\%w) -eq 0 ] && [ -x /usr/lib/zfs-linux/scrub ]; then /usr/lib/zfs-linux/scrub; fi
